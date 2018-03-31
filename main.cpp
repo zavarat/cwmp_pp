d94a9f0a188c697411c0cda6317bb78326b65f79
Alexander Tikhomirov <trivox@gmail.com>
2018-03-31 19:01:52 +0300

    Makefile, main.cpp and TODO files initially added
    They don't contain anything useful yet. Also libevent added as
    a submodule.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argrc, const char ** argv)
+{
+    // restore data from last launch
+    // get ACS data from config
+    // start http server
+    // init event_engine 
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
